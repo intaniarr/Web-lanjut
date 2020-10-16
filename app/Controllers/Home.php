b8a656d55bdfbd9d7a0b5ff4cd70623816937c24
intaniarr <rahmadhillaintan@gmail.com>
2020-10-16 18:29:30 +0700

    My project

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,4 +9,15 @@ class Home extends BaseController
 
 	//--------------------------------------------------------------------
 
+	public function show()
+	{
+		$data['nama'] = 'Intania Rahmadhilla';
+		$data['npm'] = '1817051025';
+
+		echo view('Mahasiswa/header');
+		echo view('Mahasiswa/index', $data);
+		echo view('Mahasiswa/footer');
+
+	}
+
 }

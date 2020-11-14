4bbf0b51907922911dabc3a3ea3efe07066b0ee9
intaniarr <rahmadhillaintan@gmail.com>
2020-11-14 22:51:39 +0700

    fifth commit

---
+++ app/Controllers/Produk.php
@@ -0,0 +1,16 @@
+<?php namespace App\Controllers;
+
+use Codeigneter\Controller;
+use App\Models\ProdukModel;
+
+class Produk extends BaseController
+{
+    public function index()
+    {
+
+    $model = new ProdukModel();
+    $data['data'] = $model->getProduk();
+
+    echo view ('produk/index', $data);
+    }
+}
